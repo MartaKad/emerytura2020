f12150f8a9c80e0284dac1249c6a9f4f1beadd26
MartaKad <kadluczka@gmail.com>
2020-07-21 20:16:32 +0200

    Aplikacja WinForm

---
+++ Emerytura/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxImie = new System.Windows.Forms.TextBox();
+            this.buttonWyslij = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNazwisko = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxWiek = new System.Windows.Forms.TextBox();
+            this.labelWynik = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię:";
+            // 
+            // textBoxImie
+            // 
+            this.textBoxImie.Location = new System.Drawing.Point(185, 25);
+            this.textBoxImie.Name = "textBoxImie";
+            this.textBoxImie.Size = new System.Drawing.Size(246, 31);
+            this.textBoxImie.TabIndex = 1;
+            // 
+            // buttonWyslij
+            // 
+            this.buttonWyslij.Location = new System.Drawing.Point(325, 246);
+            this.buttonWyslij.Name = "buttonWyslij";
+            this.buttonWyslij.Size = new System.Drawing.Size(106, 31);
+            this.buttonWyslij.TabIndex = 2;
+            this.buttonWyslij.Text = "Prześlij";
+            this.buttonWyslij.UseVisualStyleBackColor = true;
+            this.buttonWyslij.Click += new System.EventHandler(this.buttonWyslij_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nazwisko:";
+            // 
+            // textBoxNazwisko
+            // 
+            this.textBoxNazwisko.Location = new System.Drawing.Point(185, 81);
+            this.textBoxNazwisko.Name = "textBoxNazwisko";
+            this.textBoxNazwisko.Size = new System.Drawing.Size(246, 31);
+            this.textBoxNazwisko.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Wiek:";
+            // 
+            // textBoxWiek
+            // 
+            this.textBoxWiek.Location = new System.Drawing.Point(185, 149);
+            this.textBoxWiek.Name = "textBoxWiek";
+            this.textBoxWiek.Size = new System.Drawing.Size(100, 31);
+            this.textBoxWiek.TabIndex = 6;
+            // 
+            // labelWynik
+            // 
+            this.labelWynik.AutoSize = true;
+            this.labelWynik.Location = new System.Drawing.Point(44, 331);
+            this.labelWynik.Name = "labelWynik";
+            this.labelWynik.Size = new System.Drawing.Size(68, 25);
+            this.labelWynik.TabIndex = 7;
+            this.labelWynik.Text = "--------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 514);
+            this.Controls.Add(this.labelWynik);
+            this.Controls.Add(this.textBoxWiek);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNazwisko);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonWyslij);
+            this.Controls.Add(this.textBoxImie);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxImie;
+        private System.Windows.Forms.Button buttonWyslij;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNazwisko;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxWiek;
+        private System.Windows.Forms.Label labelWynik;
+    }
+}
+
---
+++ Emerytura/WindowsFormsApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        const int WiekEmerytalny = 65;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonWyslij_Click(object sender, EventArgs e)
+        {
+            string wynik = "Witaj " + textBoxImie.Text + " " + textBoxNazwisko.Text;
+            int wiek = int.Parse(textBoxWiek.Text);
+            string wynik2;
+            if(wiek > WiekEmerytalny)
+            {
+                wynik2 = "Jesteś emerytem";
+            }
+            else
+            {
+                wynik2 = $"Do emerytury zostało Ci {WiekEmerytalny - wiek}!";
+            }
+            labelWynik.Text = wynik + "\n" + wynik2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
